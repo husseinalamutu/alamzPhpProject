441fd78ae21c4ab526f11a7be37dcf032a79cf38
hussein alamutu <alamutuhussein@gmail.com>
2022-06-03 02:29:05 +0100

    php update

---
+++ README.md
@@ -0,0 +1,41 @@
+# Content Management System
+This Project is based on content management system build using PHP. This projects includes secured login system or signup system with admin and local user supporter. It also contain dashboard with different analytics which helps to analyse data.
+
+# Installation
+
+1. Install XAMPP or WAMPP.
+
+2. Open XAMPP Control panal and start `apache` and `mysql` .
+
+3. Download project from github [alamzPhpProject](https://github.com/husseinalamutu/alamzPhpProject.git "Content Management System")
+
+OR follow gitbash commands
+  ```sh
+  > go to xampp/htdocs
+  > open git bash
+  > git clone https://github.com/husseinalamutu/alamzPhpProject.git
+  ```
+
+4. open link localhost/phpmyadmin
+
+5. click on new at side navbar.
+
+6. give a database name as (cms) hit on create button.
+
+7. after creating database name click on import.
+
+8. browse the file in directory and In same folder there are cms.sql file.
+
+9. after importing successfully.
+
+10. open any browser and type http://localhost/php/index.php.
+
+11. first register and then login
+
+12. admin login details username = alamz and Password = softCode.
+
+## If you like my project hit a star button
+
+## Contribution
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+Please make sure to update tests as appropriate.
---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<!-- Footer -->
+<footer class="py-3 bg-dark">
+      <div class="container">
+        <p class="text-center text-white">
+          <a style="color:white" href="https://github.com/husseinalamutu"><i class="fab fa-github fa-2x mr-3"></i></a>
+          <a style="color:white" href="https://www.linkedin.com/in/hussein-alamutu"><i class="fab fa-linkedin fa-2x mr-3"></i></a>
+          <a style="color:white" href="https://www.instagram.com/husseinalamutu"><i class="fab fa-instagram fa-2x mr-3"></i></a>
+          <a style="color:white" href="https://www.twitter.com/husseinalamz"><i class="fab fa-twitter fa-2x mr-3"></i></a>
+          <br><br>&copy; <?php echo date("Y");?> |
+          Designed By <a href="https://github.com/husseinalamutu" style="color:yellow">Hussein Alamutu</a>
+        </p>
+      </div>
+      <!-- /.container -->
+</footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+  </body>
+
+</html>
